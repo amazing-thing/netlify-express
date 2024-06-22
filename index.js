bf6b022eea7a7c9843a16ed76c310ca84222f983
神奇的是 <1821238705@qq.com>
2024-06-22 17:40:33 +0800

    chore: 入口提示调整

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(cors())
 // 获取小说
 app.get('/', (req, res) => {
   res.send(`<div>一个简单的应用</div>
-    <div>从此地址寻找对应的小说，然后进行拼接跳转，主要是修改地址末尾的数字（171274）为对应的小说即可。列如：</div>
-    <div>https://netlify-express-ten.vercel.app/book?url=https://www.wwscdh.com/html/171274</div>
+    <div>从此地址（https://www.wwscdh.com）寻找对应的小说，然后进行拼接跳转，主要是修改地址末尾的数字（171274）为对应的小说即可。列如：</div>
+    <div>https://netlify-express-ten.vercel.app/book?url=https://www.wwscdh.com/html/<span style="color:red">171274</span></div>
     `)
 })
 https://www.wwscdh.com
